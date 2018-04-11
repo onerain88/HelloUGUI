8498e64db24d8c1fe2a984ea6e9540924982ed29
onerain <onerain88@gmail.com>
2018-04-11 18:27:56 +0800

    初始化工程，包括 尺寸自适应和聊天对话框示例

---
+++ Assets/Chat/ChatItem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatItem : MonoBehaviour {
+    public Sprite[] avatars = null;
+
+    public Image avatarImage = null;
+    public Text chatText = null;
+
+    public void setChat(string chat) {
+        int index = Random.Range(0, avatars.Length);
+        this.avatarImage.sprite = this.avatars[index];
+        this.chatText.text = chat;
+    }
+}
---
+++ Assets/Chat/ChatUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatUI : MonoBehaviour {
+    public ScrollRect chatScrollRect = null;
+    public Transform chatContentTransform = null;
+    public InputField chatInputField = null;
+
+    public void onSendButtonClicked() {
+        string chat = this.chatInputField.text;
+        string chatItemRes = Random.Range(0, 2) == 0 ? "LeftChatItem" : "RightChatItem";
+        GameObject chatItemGO = Instantiate(Resources.Load(chatItemRes), chatContentTransform) as GameObject;
+        ChatItem chatItem = chatItemGO.GetComponent<ChatItem>();
+        chatItem.setChat(chat);
+
+        StartCoroutine(goToBottom());
+    }
+
+    IEnumerator goToBottom() {
+        yield return null;
+        this.chatScrollRect.normalizedPosition = Vector2.zero;
+    }
+}
